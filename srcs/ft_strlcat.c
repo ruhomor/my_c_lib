14fd48a2ed6f686a51d3568a45fac51a1d8618e6
ruhomor <ruhomor@icloud.com>
2019-09-15 20:25:22 +0300

    a

--- srcs/ft_strlcat.c
+++ srcs/ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: kachiote <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/15 18:02:20 by kachiote          #+#    #+#             */
-/*   Updated: 2019/09/15 19:26:34 by kachiote         ###   ########.fr       */
+/*   Updated: 2019/09/15 20:24:53 by kachiote         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,24 @@
 
 size_t	ft_strlcat(char *dst, const char *src, size_t size)
 {
-	while ((*dst) && ())
+	size_t dstsize;
+	size_t cpysize;
+
+	dstsize = 0;
+	while (*dst)
+	{
+		size--;
+		dst++;
+		dstsize++;
+	}
+	cpysize = 0;
+	while ((*src) && (size > 1))
+	{
+		*dst = *src++;
+		dst++;
+		cpysize++;
+		size--;
+	}
+	*dst = '\0';
+	return (dstsize + cpysize);
 }
