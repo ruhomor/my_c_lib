e21f07e8e5dfdeb12dd72caa5cca3c71aa1e0919
ruhomor <ruhomor@icloud.com>
2019-09-17 20:42:07 +0300

    a

--- srcs/ft_strlcat.c
+++ srcs/ft_strlcat.c
@@ -6,32 +6,31 @@
 /*   By: kachiote <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/15 18:02:20 by kachiote          #+#    #+#             */
-/*   Updated: 2019/09/15 20:24:53 by kachiote         ###   ########.fr       */
+/*   Updated: 2019/09/17 20:40:06 by kachiote         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t	ft_strlcat(char *dst, const char *src, size_t size)
+size_t  ft_strlcat(char *dst, const char *src, size_t size)
 {
-	size_t dstsize;
-	size_t cpysize;
+	size_t  dstlen;
+	size_t  srclen;
 
-	dstsize = 0;
-	while (*dst)
+	dstlen = 0;
+	srclen = ft_strlen(src);
+	while ((*dst) && (size))
 	{
-		size--;
 		dst++;
-		dstsize++;
-	}
-	cpysize = 0;
-	while ((*src) && (size > 1))
-	{
-		*dst = *src++;
-		dst++;
-		cpysize++;
+		dstlen++;
 		size--;
 	}
-	*dst = '\0';
-	return (dstsize + cpysize);
+	while (*src && size > 1)
+	{
+		*dst++ = *src++;
+		size--;
+	}
+	if (size == 1 || *src == 0)
+		*dst = '\0';
+	return (srclen + dstlen);
 }
