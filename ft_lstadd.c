1e79769a896c55882eeacf72f719507e668b1104
Kathern Achiote <kachiote@at-i5.msk.21-school.ru>
2020-07-03 15:05:12 +0300

    conflicting name

--- ft_lstadd.c
+++ ft_lstadd.c
@@ -6,17 +6,17 @@
 /*   By: kachiote <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/23 17:00:14 by kachiote          #+#    #+#             */
-/*   Updated: 2019/09/23 17:21:09 by kachiote         ###   ########.fr       */
+/*   Updated: 2020/07/03 14:48:48 by kachiote         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_lstadd(t_list **alst, t_list *new)
+void	ft_lstadd(t_list **alst, t_list *neww)
 {
 	t_list	*meme;
 
 	meme = *alst;
-	*alst = new;
-	new->next = meme;
+	*alst = neww;
+	neww->next = meme;
 }
